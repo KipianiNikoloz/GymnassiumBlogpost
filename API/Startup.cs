2632f5082f0920a34b36920a6a9ca56bcfd1e249
KipianiNikoloz <nikolozkipiani2005@gmail.com>
2021-12-22 02:28:51 +0400

    testing heroku

--- API/Startup.cs
+++ API/Startup.cs
@@ -60,6 +60,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
